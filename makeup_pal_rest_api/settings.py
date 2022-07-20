103b6a7e243b361260e4c94ec752b99b83abbaeb
Victoria Le <leqvictoria@gmail.com>
2022-07-20 12:14:50 -0400

    superuser created

--- makeup_pal_rest_api/settings.py
+++ makeup_pal_rest_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appts_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'makeup_pal_rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'makeup_pal_api',
+        'USER':'',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
