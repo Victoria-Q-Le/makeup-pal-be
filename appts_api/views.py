fa858cadaa62604f6aac3176c97815dac8404ffd
Victoria Le <leqvictoria@gmail.com>
2022-07-20 19:21:38 -0400

    views finished

--- appts_api/views.py
+++ appts_api/views.py
@@ -3,3 +3,13 @@ from rest_framework import generics
 from .serializers import ApptSerializer
 from .models import Appt
 # Create your views here.
+
+class ApptList(generics.ListCreateAPIView):
+    queryset = Appt.objects.all().order_by('id')
+    serializer_class = ApptSerializer
+# this is for post and get
+
+class ApptDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Appt.object.all().order_by('id')
+    serializer_class = ApptSerializer
+#get, delete, put
