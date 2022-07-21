737079cf85e20885e4f785709f25e03436df9184
Victoria Le <leqvictoria@gmail.com>
2022-07-20 21:21:15 -0400

    successfully connected to frontend

--- appts_api/urls.py
+++ appts_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-url =[
+urlpatterns =[
     path('api/appts', views.ApptList.as_view(), name = 'appt_list'),
     path('api/appts/<int:pk>', views.ApptDetail.as_view(), name = 'appt_detail'),
 ]
--- appts_api/views.py
+++ appts_api/views.py
@@ -10,6 +10,6 @@ class ApptList(generics.ListCreateAPIView):
 # this is for post and get
 
 class ApptDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Appt.object.all().order_by('id')
+    queryset = Appt.objects.all().order_by('id')
     serializer_class = ApptSerializer
 # get, delete, put
